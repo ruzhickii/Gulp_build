92e555c099f4f7d16730836053c16e7af4353f00
ruzhickii <ruzhickiisasha@gmail.com>
2017-08-23 16:39:24 +0300

    first gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
-   gulp.src('./src/**/*.scss')
+   gulp.src('./src/scss/*.scss')
        .pipe(sass().on('error', sass.logError))
-       .pipe(gulp.dest('./src/build'));
+       .pipe(gulp.dest('./build'))
+       .pipe(autoprefixer({
+           browsers: ['last 2 versions'],
+           cascade: false,
+           flexbox: "no-2009"
+       }))
 });
 
 gulp.task('sass:watch', function () {
-   gulp.watch('./src/**/*.scss', ['sass']);
+   gulp.watch('./src/scss/*.scss', ['sass']);
 });
